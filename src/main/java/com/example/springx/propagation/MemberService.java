cdfc143d1836d7db368239d12f47823b092f3b6e
minu <made_power@naver.com>
2023-05-17 17:29:27 +0900

    트랜잭션 전파 복구2
    로그를 남기는 건 실패했지만 회원가입은 진행시키려면 @Transactional 옵션을 Requires_New 옵션을 주어서 해결해보았다. 이 경우 트랜잭션을 분리시킨 것이다. 커넥션을 별도로 사용하게 된다.

--- src/main/java/com/example/springx/propagation/MemberService.java
+++ src/main/java/com/example/springx/propagation/MemberService.java
@@ -3,6 +3,7 @@ package com.example.springx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
@@ -24,7 +25,7 @@ public class MemberService {
         logRepository.save(logMessage);
         log.info("==logRepository 호출 종료");
     }
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void joinV2(String username){
         Member member = new Member(username);
         Log logMessage = new Log(username);
