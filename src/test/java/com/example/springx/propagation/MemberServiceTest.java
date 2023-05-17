f16b8039a377eeac9355b2b5ea8ed7889d8f79c0
minu <made_power@naver.com>
2023-05-17 17:12:00 +0900

    트랜잭션 전파 롤백
    하나의 논리 트랜잭션이라도 롤백 된다면, 물리 트랜잭션은 롤백된다.

--- src/test/java/com/example/springx/propagation/MemberServiceTest.java
+++ src/test/java/com/example/springx/propagation/MemberServiceTest.java
@@ -56,10 +56,20 @@ class MemberServiceTest {
         String username = "outerTxOn_success";
         //when
         memberService.joinV1(username);
-        //then: 모든 데이터가 정상 저장된다. 
+        //then: 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
     }
-
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username = "로그예외_outerTxOn_fail";
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+        //then: 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
 
 }
